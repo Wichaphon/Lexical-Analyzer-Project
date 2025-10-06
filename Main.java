881b9a89ec0d812a0267148bde65e9be16188ec3
Wichaphon <horokindov3310@gmail.com>
2025-10-06 17:39:43 +0700

    add Main.java for test reading file

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class Main {
     public static void main(String[] args) {
-        
+        if (args.length == 0) {
+            System.err.println("Usage: java Main <input_file>");
+            return;
+        }
+
+        try (Reader reader = new FileReader(args[0])) {
+            Lexer lexer = new Lexer(reader);
+            lexer.yylex(); 
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
     }
 }
